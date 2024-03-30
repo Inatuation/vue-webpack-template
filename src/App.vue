5b6f1ad4de80e9bdb1a559a38882159bcd591c6d
Infatuation <5252107+infatuation5858@user.noreply.gitee.com>
2024-03-30 19:52:09 +0800

    commit: hbs commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="app_body">
         <home></home>
+        <el-button>测试按钮</el-button>
     </div>
 </template>
 
 <script setup>
 import home from './pages/home/index.vue';
-
+import { ElButton } from 'element-plus';
 </script>
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-{{importAxiosPlugin}}
-
-const app = createApp(App);
-{{useAxiosPlugin}}
-app.mount('#app');
